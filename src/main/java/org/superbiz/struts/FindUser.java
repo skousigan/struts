4a0a0f5bd2850ebe98e506c4f7350e607e5e75dc
Kousigan Srinivasan <kousigan.srinivasan@cognizant.com>
2020-04-16 09:33:40 +0530

    Replatforming Struts

--- src/main/java/org/superbiz/struts/FindUser.java
+++ src/main/java/org/superbiz/struts/FindUser.java
@@ -17,12 +17,15 @@
  */
 package org.superbiz.struts;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import java.util.Properties;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FindUser {
+    private final UserService service;
 
+    public FindUser(UserService service) {
+        this.service = service;
+    }
     private int id;
     private String errorMessage;
     private User user;
@@ -54,12 +57,6 @@ public class FindUser {
     public String execute() {
 
         try {
-            UserService service = null;
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.apache.openejb.core.LocalInitialContextFactory");
-            Context ctx = new InitialContext(props);
-            service = (UserService) ctx.lookup("UserServiceImplLocal");
             this.user = service.find(id);
         } catch (Exception e) {
             this.errorMessage = e.getMessage();
